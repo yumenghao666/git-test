109be4a961648a4559d2eb606e2063397598710c
missyu <1584438665@qq.com>
2019-06-29 22:44:46 +0800

    把本地工程添加到git.

---
+++ src/main/java/com/itheima/git/FirstClass.java
@@ -0,0 +1,15 @@
+package com.itheima.git;
+
+/**
+ * @Description
+ * @auther lenovo
+ * @create 2019-06-29 22:22
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+}
