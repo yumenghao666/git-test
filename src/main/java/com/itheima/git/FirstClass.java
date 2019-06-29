c74b39bd35b99dfa656bbd4821d68a3fd90b014b
missyu <1584438665@qq.com>
2019-06-29 22:48:46 +0800

    第二次修改数据.

--- src/main/java/com/itheima/git/FirstClass.java
+++ src/main/java/com/itheima/git/FirstClass.java
@@ -12,4 +12,10 @@ public class FirstClass {
         System.out.println("hello");
         System.out.println("hello");
     }
+
+    public void test() {
+        int i = 0;
+         i+= 10;
+        System.out.println(i);
+    }
 }
